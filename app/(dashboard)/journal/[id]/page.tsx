75fa9aec5bef9fd79cb01d7bb0de3144735524b1
Saad Zafar <saadzafar.code@gmail.com>
2024-12-24 08:01:09 +0100

    decommissioning db key

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -29,9 +29,34 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => {
   const entry = await getEntry(params.id)
+  const analysisData = [
+    { name: 'Summary', value: '' },
+    { name: 'Subjec', value: '' },
+    { name: 'Mood', value: '' },
+    { name: 'Negative', value: 'False' },
+  ]
+
   return (
-    <div className="h-full w-full">
-      <Editor entry={entry} />
+    <div className="h-full w-full grid grid-cols-3">
+      <div className="col-span-2">
+        <Editor entry={entry} />
+      </div>
+      <div className="border-l border-black/10">
+        <div className="bg-blue-600 px-6 py-10">
+          <h2 className="text-2xl">Analysis</h2>
+        </div>
+        <ul>
+          {analysisData.map((item) => (
+            <li
+              className="flex items-center justify-between px-2 py-4 border-b border-t border-black/10"
+              key={item.name}
+            >
+              <span className="text-lg font-semibold">{item.name}</span>
+              <span>{item.value}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
