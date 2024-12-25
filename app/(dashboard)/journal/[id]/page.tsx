8c648e072d1a5fa0c94ec06ce55da23f5af6fcf0
Saad Zafar <saadzafar.code@gmail.com>
2024-12-25 06:53:38 +0100

    expanded editor, data passing issue

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -32,35 +32,11 @@ const getEntry = async (id) => {
 
 const EntryPage = async ({ params }) => {
   const entry = await getEntry(params.id)
-  const { mood, subject, summary, color, negative } = entry?.analysis
-  const analysisData = [
-    { name: 'Summary', value: summary },
-    { name: 'Subject', value: subject },
-    { name: 'Mood', value: mood },
-    { name: 'Negative', value: negative ? 'True' : 'False' },
-  ]
 
   return (
-    <div className="h-full w-full grid grid-cols-3">
-      <div className="col-span-2">
-        <Editor entry={entry} />
-      </div>
-      <div className="border-l border-black/10">
-        <div className="px-6 py-10" style={{ backgroundColor: color }}>
-          <h2 className="text-2xl">Analysis</h2>
-        </div>
-        <ul>
-          {analysisData.map((item) => (
-            <li
-              className="flex items-center justify-between px-2 py-4 border-b border-t border-black/10"
-              key={item.name}
-            >
-              <span className="text-lg font-semibold">{item.name}</span>
-              <span>{item.value}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className="h-full w-full">
+      <Editor entry={entry} />
+      {/* using editor as the overall page, FutureFix alpha-coconut */}
     </div>
   )
 }
