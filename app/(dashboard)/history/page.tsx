8616699eb6b0a527dce0d1cae64bb763f7c953e0
Saad Zafar <saadzafar.code@gmail.com>
2024-12-26 02:56:17 +0100

    sentiment history section added

--- app/(dashboard)/history/page.tsx
+++ app/(dashboard)/history/page.tsx
@@ -1,5 +1,38 @@
-const History = () => {
-  return <div>History</div>
+import HistoryChart from '@/components/HistoryChart'
+import { getUserByClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getData = async () => {
+  const user = await getUserByClerkID()
+  const analyses = await prisma.analysis.findMany({
+    where: {
+      userId: user.id,
+    },
+    // getting rid of select because we will use everything
+    // select: {
+    //   sentimentScore: true,
+    // },
+    orderBy: {
+      createdAt: 'asc',
+    },
+  })
+
+  const sum = analyses.reduce((all, current) => all + current.sentimentScore, 0)
+  const avg = Math.round(sum / analyses.length)
+  return { analyses, avg }
+}
+
+const History = async () => {
+  const { avg, analyses } = await getData()
+  console.log(analyses)
+  return (
+    <div className="w-full h-full">
+      <div>{`Average Sentiment: ${avg}`}</div>
+      <div className="w-full h-full">
+        <HistoryChart data={analyses} />
+      </div>
+    </div>
+  )
 }
 
 export default History
